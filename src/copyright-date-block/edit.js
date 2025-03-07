464be22c7539bc79b125a067710ebf5a32b2b683
Vladimir Kamuz <v.kamuz@gmail.com>
2025-03-07 12:29:10 +0200

    Added startingYear attribute

--- src/copyright-date-block/edit.js
+++ src/copyright-date-block/edit.js
@@ -11,7 +11,8 @@ import { __ } from '@wordpress/i18n';
  *
  * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
  */
-import { useBlockProps } from '@wordpress/block-editor';
+import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { PanelBody, TextControl } from '@wordpress/components';
 
 /**
  * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
@@ -29,14 +30,22 @@ import './editor.scss';
  *
  * @return {Element} Element to render.
  */
-export default function Edit() {
+export default function Edit( props ) {
+	const startingYear = props.attributes.startingYear;
 	const currentYear = new Date().getFullYear().toString();
 	return (
-		<p { ...useBlockProps() }>
-			{ __(
-				'Copyright',
-				'copyright-date-block'
-			) } &copy; 2019 - { currentYear }
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody title="Settings">
+					Settings
+				</PanelBody>
+			</InspectorControls>
+			<p { ...useBlockProps() }>
+				{ __(
+					'Copyright',
+					'copyright-date-block'
+				) } &copy; { startingYear } - { currentYear }
+			</p>
+		</>
 	);
 }
